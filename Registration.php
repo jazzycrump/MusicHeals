00efc5537c9554e0ffd0c51d043565be9c4361a1
jazzycrump <46763999+jazzycrump@users.noreply.github.com>
2019-03-13 14:23:36 -0400

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -7,10 +7,8 @@ $zip = $_POST["zip"];
 
 print "$name  $phone  $city  $zip<br/>";
 
-print "<h3>Validating test field using different patterns</h3>\n";
-
 print "<h3>Validating name</h3>\n";
-// The "i" after the pattern delimiter indicates a case-insensitive search
+
 if (!preg_match("/^[a-z]+,[ ]*[a-z]+$/i", $name)) {
 	print "Invalid name<br/>";
 }
@@ -19,8 +17,7 @@ else {
 }
 
 print "<h3>Validating phone number</h3>\n";
-// Parentheses are backslashed since they have a special meaning 
-// in regular expression otherwise
+
 if (!preg_match("/^\([0-9]{3}\)[ ]?[0-9]{3}-[0-9]{4}$/", $phone)) {
 	print "Invalid phone number<br/>";
 }
@@ -28,8 +25,6 @@ else {
 	print "The phone number is valid<br/>";
 }
 
-// Here the match is case-sensitive to restrict states to uppercase only
-// [ ]* - matches zero or more spaces between  the coma and the state 
  print "<h3> Validating City and State</h3>";
  if (!preg_match('/^[A-Za-z]+,[ ]*[A-Z]{2}$/', $city))  {
 print "Invalid city and state<br/>";
@@ -46,8 +41,7 @@ if(!preg_match('/^[0-9]{5}$/', $zip))   {
 else  {
         print "The zip code is valid <br />";
 }	
-
-
 ?>
-
 </div>
+</body>
+</html>
