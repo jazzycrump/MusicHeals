6050c1726e71ac880655f41dc8ce250d879344e6
jazzycrump <46763999+jazzycrump@users.noreply.github.com>
2019-04-15 15:53:14 -0400

    Add files via upload

--- Controller/Login-Controller.php
+++ Controller/Login-Controller.php
@@ -1,14 +1,39 @@
 <?php
-/** if $_POST['user'] = login
-{
-      
+/* Displays user information and some useful messages */
+
+// Check if user is logged in using the session variable
+if ( $_SESSION['logged_in'] != 1 ) {
+  $_SESSION['message'] = "You must log in before viewing your profile page!";
+  header("location: error.php");    
 }
-        
-        location('loginm.php')
-        location('error.php')
-/**if (!empty($_POST['user']) && !empty($_POST['pass'])){
-    header("location: loginm.php");
+else {
+    $first_name = $_SESSION['first_name'];
+    $last_name = $_SESSION['last_name'];
+    $email = $_SESSION['email'];
+    $active = $_SESSION['active'];
+}
 ?>
- * */
- 
-?>
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Welcome <?= $first_name.' '.$last_name ?></title>
+</head>
+
+<body>
+  <div class="form">
+
+          <h1>Welcome</h1>
+          
+          
+          <h2><?php echo $first_name.' '.$last_name; ?></h2>
+          
+          
+          <p><?= $email ?></p>
+          
+          <a href="logout.php"><button type = 'submit' name="logout"/>Log Out</button></a>
+
+    </div>
+
+</body>
+</html>
