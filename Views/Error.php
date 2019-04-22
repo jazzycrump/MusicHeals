f6fe2aa32b3ea32d6ca96c915de4e60d9e46b651
jazzycrump <46763999+jazzycrump@users.noreply.github.com>
2019-04-22 17:28:02 -0400

    Add files via upload

---
+++ Views/Error.php
@@ -0,0 +1,18 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<!--
+This error page will be used if a potential Music Heals user tries to login and
+ does not have an account under the username.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Error Page</title>
+    </head>
+    <body
+        <h1> Error </h1>
+        <br>
+        <p>Sorry, this username was not found. Please try again.</p>
+        
+    </body>
+</html>
--- Views/Homepage.php
+++ Views/Homepage.php
@@ -1,100 +1,24 @@
-
-/*This code is the login and signup page for MusicHeals which allows the user to 
- login to music heals and if they do not have an account they may sign up for one.*/
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Sign-Up and Login</title>
-</head>
-
-/*This method that will be utilized is a post method 
-so that when information is entered it will post to the php page.*/
-/* Use server[request method] to check for a form submission. */
-<?php 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') 
-{
-    /*Sends user information to login.php*/
-    if (isset($_POST['login'])) {
-
-        require 'Login Model.php';
-    /*Sends user information to registration.php */
-    }
-    elseif (isset($_POST['register'])) {
-        
-        require 'Register Model.php';
-        
-    }
+<?php session_start();
+require_once("models/UserModel.php");
+if (!User::isUserLoggedIn()) {
+    header("location:http://localhost/index.php");
 }
 ?>
-<body>
-  <div class="form">
-      
-      <ul>
-        <li>><a href="#signup">Sign Up</a></li>
-        <li> <a href="#login">Log In</a></li>
-      </ul>
-      
-      
-
-         <div id="login">   
-          <h1>Welcome To Music Heals!</h1>
-          
-          <form action="Homepage.php" method="post" autocomplete="off">
-              
-          /* This is the login page and is the first page you will see on the Music Heals website.*/
-          
-            
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email" name="email"/>
-          </div>
-          
-         
-            <label>
-              Password<span class="req">*</span>
-            </label>
-            <input type="password" required autocomplete="off" name="password"/>
-          
-          
-          <button type='submit' name="login" />Log In</button>
-
-        </div>
-        <div id="signup">   
-          <h1>Sign Up for MusicHeals</h1>
-          
-          <form action="Homepage.php" method="post" autocomplete="off">
-          
-          
-              <label>
-                First Name<span class="req">*</span>
-              </label>
-              <input type="text" name='firstname'/>
-            </div>
-        
-            
-              <label>
-                Last Name<span class="req">*</span>
-              </label>
-              <input type="text"required autocomplete="off" name='lastname' />
-           
-        
-
-          
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off" name='email' />
-          
-          
-          
-            <label>
-              Make up a Password<span class="req">*</span>
-            </label>
-            <input type="password"required autocomplete="off" name='password'/>
-         
-          
-          <button type="submit"name="register" />Register</button>
-</body>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Homepage</title>
+    </head>
+    <body>
+        <?php $user = User::getSessionUser(); ?>
+        <h1> Welcome Back </h1> <?php echo $user->getMHUserName(); ?>
+        <br>
+        <p> </p>
+    </body>
 </html>
-
