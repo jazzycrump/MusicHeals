a489b68805f33dc9235630dc6fae6694055348ea
jazzycrump <46763999+jazzycrump@users.noreply.github.com>
2019-04-22 17:32:28 -0400

    Update Homepage.php

--- Views/Homepage.php
+++ Views/Homepage.php
@@ -5,11 +5,9 @@ if (!User::isUserLoggedIn()) {
 }
 ?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
+
+
 <html>
     <head>
         <meta charset="UTF-8">
