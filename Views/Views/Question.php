8295b41731573182528440a0f6946749cb6fb053
jazzycrump <46763999+jazzycrump@users.noreply.github.com>
2019-04-24 17:31:17 -0400

    Update Question.php

--- Views/Views/Question.php
+++ Views/Views/Question.php
@@ -11,11 +11,15 @@ This error page will be used if a potential Music Heals user tries to login and
     </head>
 
 <body>
- <input type="radio" name="question1" <?php if (isset($question1) && $gender=="yes") echo "checked";?> value="yes">Yes
-  <input type="radio" name="question1" <?php if (isset($question1) && $gender=="no") echo "checked";?> value="no">No
+ <form>
+ <input type="radio" name="question1" <?php if (isset($question1) && $question1=="yes") echo "checked";?> value="yes">Yes
+  <input type="radio" name="question1" <?php if (isset($question1) && $question1=="no") echo "checked";?> value="no">No
 
 
-  <input type="radio" name="question2" <?php if (isset($question2) && $gender=="yes") echo "checked";?> value="yes">Yes
-  <input type="radio" name="question2" <?php if (isset($question2) && $gender=="no") echo "checked";?> value="no">No
+  <input type="radio" name="question2" <?php if (isset($question2) && $question2=="yes") echo "checked";?> value="yes">Yes
+  <input type="radio" name="question2" <?php if (isset($question2) && $question2=="no") echo "checked";?> value="no">No
   
+ 
+ <input type = "submit" name="submit" value="Submit">
+ </form>
 </body>
