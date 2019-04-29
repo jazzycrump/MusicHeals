0c0e70404a8ba84cf8e69673493cabc1dfbdff42
jazzycrump <46763999+jazzycrump@users.noreply.github.com>
2019-04-29 15:09:22 -0400

    Controller

---
+++ Controller/QuestionController.php
@@ -0,0 +1,15 @@
+<?php session_start();
+
+require_once("../Models/QuestionModel.php");
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    
+    if ($_POST['form-type'] == "submit") {
+        // Call up user model and login user.
+        if ($submit)  {
+            header("location:http://localhost/Music.html");
+        } else {
+            header("location:http://localhost/Error.php");
+        }
+    }
+}
