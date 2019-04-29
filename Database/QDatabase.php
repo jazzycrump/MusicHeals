74838a41dc8f40b926fc751aef595f12bbc1a83e
jazzycrump <46763999+jazzycrump@users.noreply.github.com>
2019-04-29 15:08:09 -0400

    Database

---
+++ Database/QDatabase.php
@@ -0,0 +1,21 @@
+<?php
+
+class QDatabase {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+    public static function loadQuestions() {
+$file = fopen("questions.txt","r") or die("Unable to open file");
+while(! feof($file)) {
+  echo fgets($file). "<br />";
+  }
+fclose($file);
+    }
+}
+?>
+    <form action = "..\..\API\Youtube.php" method='post'>
+    <button type='submit'>Get Results</button>
+    </form>
+            
