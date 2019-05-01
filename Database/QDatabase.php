8aa0aba2d16e10b95c5f21b77a96ae68f269d011
jazzycrump <46763999+jazzycrump@users.noreply.github.com>
2019-05-01 17:18:39 -0400

    Add files via upload

--- Database/QDatabase.php
+++ Database/QDatabase.php
@@ -1,21 +1,34 @@
+<form action = "../API/Youtube.php" method='post'>
 <?php
 
-class QDatabase {
+//class QDatabase {
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-    public static function loadQuestions() {
-$file = fopen("questions.txt","r") or die("Unable to open file");
-while(! feof($file)) {
-  echo fgets($file). "<br />";
-  }
-fclose($file);
+    function loadQuestions() {
+        $count = 1;
+
+        $file = fopen("/Users/Jazz/Documents/Music/Data/questions","r") or die("Unable to open file");
+        while(! feof($file)) {
+            echo $count.") ". fgets($file). "<br/>";
+            $attrName = "question".strval($count)."a";
+            $attrName2 = "question".strval($count)."b";
+            $clssName = "radio".strval($count);
+            echo "<input type='checkbox' id='$attrName' value= 'yes' name='$attrName' class='$clssName'>Yes</input></tr>"."<br/>";
+            echo "<input type='checkbox' id='$attrName2' value='no' name='$attrName2' class='$clssName'>No</input></tr>"."<br/>";
+            echo "<br/>";
+            $count++;
+
+        }
+        fclose($file);
+
     }
-}
+
+    loadQuestions();
+//}
+
 ?>
-    <form action = "..\..\API\Youtube.php" method='post'>
-    <button type='submit'>Get Results</button>
-    </form>
-            
+<input type='submit'>Get Results</input>
+</form>
